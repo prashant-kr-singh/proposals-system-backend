66e98c50c60494b6b9544ec78aa718bcd78e9670
ks8346 <kartik.sachdeva@soprabanking.com>
2020-11-03 17:48:06 +0530

    merging

--- src/main/java/com/soprabanking/ips/controllers/UpvotesController.java
+++ src/main/java/com/soprabanking/ips/controllers/UpvotesController.java
@@ -8,6 +8,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,7 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.soprabanking.ips.models.Upvotes;
 import com.soprabanking.ips.services.UpvotesService;
-
+@CrossOrigin
 @RestController
 @RequestMapping("/upvotes")
 public class UpvotesController
